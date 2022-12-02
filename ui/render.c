5c1e003bfc5aca5226c12ca98516f893e865c583
BLOODWIING <bandym555@gmail.com>
2022-12-02 12:49:15 +0200

    Text rendering

--- ui/render.c
+++ ui/render.c
@@ -2,16 +2,29 @@
 #include "con_lib.h"
 #include <stdio.h>
 
+void renderText(wchar_t * text) {
+    wprintf(L"%ls", text);
+}
+
+void renderTextColoured(UserSettings * settings, int fg, int bg, wchar_t * text) {
+    if (settings->display.colourful == true)
+        con_set_color(bg, fg);
+
+    renderText(text);
+
+    if (settings->display.colourful == true)
+        con_set_color(COLOR_RESET, COLOR_RESET);
+}
+
 void renderPiece(UserSettings * settings, Team * team, Piece * piece) {
-    if (settings->display.colourful == true) {
-        con_set_color(COLOR_BLACK, team->colour);
-    }
+    if (settings->display.colourful == true)
+        con_set_color(COLOR_RESET, team->colour);
 
-    if (settings->display.unicode == true) {
+    if (settings->display.unicode)
         wprintf(L"%ls", piece->unicode);
-    } else {
+    else
         wprintf(L"%c", piece->symbol);
-    }
 
-    con_set_color(COLOR_BLACK, COLOR_GRAY);
+    if (settings->display.colourful == true)
+        con_set_color(COLOR_RESET, COLOR_RESET);
 }
--- ui/render.h
+++ ui/render.h
@@ -5,6 +5,8 @@
 #include "../model/piece.h"
 #include "../model/team.h"
 
+void renderText(wchar_t * text);
+void renderTextColoured(UserSettings * settings, int fg, int bg, wchar_t * text);
 void renderPiece(UserSettings * settings, Team * team, Piece * piece);
 
 #endif //CHESS_RENDER_H
