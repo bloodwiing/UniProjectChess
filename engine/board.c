fa670a3663d7a9f7bc001106fe27f5e707a151f2
BLOODWIING <bandym555@gmail.com>
2022-12-04 22:21:47 +0200

    Quick refactor

--- engine/board.c
+++ engine/board.c
@@ -1,24 +1,22 @@
 #include "board.h"
-#include "con_lib.h"
+#include "../ui/con_lib.h"
 #include <string.h>
-#include "render.h"
-#include "../engine/validation.h"
+#include "../ui/render.h"
+#include "validation.h"
 #include <wchar.h>
 
-Board * createBoard(Scenario * scenario, UserSettings * settings) {
+Board * createEmptyBoard(Scenario * scenario, UserSettings * settings) {
     Board * out = malloc(sizeof(Board));
 
     out->user_settings = settings;
+    out->scenario = scenario;
 
     out->width = scenario->size_x;
     out->height = scenario->size_y;
 
-    out->tiles = calloc(scenario->size_x * scenario->size_y, sizeof(Piece *));
-    for (int i = 0; i < scenario->spawn_count; i++) {
-        Spawn * spawn = scenario->spawns + i;
-        Team * team = scenario->teams + spawn->team;
-        Piece * piece = team->pieces + spawn->type;
-        out->tiles[spawn->x + spawn->y * scenario->size_x] = piece;
+    out->tiles = calloc(scenario->size_x * scenario->size_y, sizeof(Tile *));
+    for (int i = 0; i < scenario->size_x * scenario->size_x; ++i) {
+        out->tiles[i] = calloc(1, sizeof(Tile));
     }
 
     out->team_count = scenario->team_count;
@@ -26,7 +24,20 @@ Board * createBoard(Scenario * scenario, UserSettings * settings) {
     for (int i = 0; i < scenario->team_count; i++)
         out->teams[i] = scenario->teams[i];
 
-    out->turn = 0;
+    return out;
+}
+
+Board * createBoard(Scenario * scenario, UserSettings * settings) {
+    Board * out = createEmptyBoard(scenario, settings);
+
+    for (int i = 0; i < scenario->spawn_count;) {
+        Spawn * spawn = scenario->spawns + i++;
+        Team * team = scenario->teams + spawn->team;
+        Piece * piece = team->pieces + spawn->type;
+        out->tiles[spawn->x + spawn->y * scenario->size_x]->game_piece = createGamePiece(piece, spawn->type);
+    }
+
+    out->active_turn = 1;
 
     return out;
 }
@@ -73,9 +84,10 @@ void renderBoardWithSelection(Board * board, int pos_x, int pos_y, int i, int j,
                 if (sel_x != -1 && sel_y != -1)
                     valid = validatePieceAnyMove(board, sel_x, sel_y, i / 2, j);
 
-                Piece *piece;
-                if ((piece = board->tiles[tile]) != NULL) {
-                    renderPieceWithBackground(board->user_settings, board->teams + piece->team, piece, valid ? COLOR_GREEN : COLOR_RESET);
+                GamePiece * game_piece;
+                if ((game_piece = board->tiles[tile]->game_piece) != NULL) {
+                    renderPieceWithBackground(board->user_settings, board->teams + game_piece->team,
+                                              getOriginalPiece(game_piece, board->scenario), valid ? COLOR_GREEN : COLOR_RESET);
                 } else {
                     renderTextColoured(board->user_settings, valid ? COLOR_GREEN : COLOR_RESET, COLOR_LIGHT_GRAY, L" ");
                 }
@@ -100,4 +112,29 @@ void renderScenario(Scenario * scenario, UserSettings * settings, int pos_x, int
     Board * board = createBoard(scenario, settings);
     renderBoard(board, pos_x, pos_y, i, j, w, h);
     free(board);
+}
+
+void saveBoard(Board * board, FILE * stream) {
+    saveScenario(board->scenario, stream);
+    for (int i = 0; i < board->width * board->height;) {
+        saveTile(board->tiles[i++], stream);
+    }
+    fwrite(&board->active_turn, sizeof(uint8_t), 1, stream);
+}
+
+Board * loadBoard(UserSettings * settings, FILE * stream) {
+    Scenario * scenario = loadScenario(stream);
+    Board * out = createEmptyBoard(scenario, settings);
+
+    for (int i = 0; i < scenario->size_x * scenario->size_y; i++) {
+        Tile * tile = loadTile(stream);
+        if (tile->game_piece != NULL) {
+            out->tiles[i]->game_piece = tile->game_piece;
+        }
+        free(tile);
+    }
+
+    fread(&out->active_turn, sizeof(uint8_t), 1, stream);
+
+    return out;
 }
--- engine/board.h
+++ engine/board.h
@@ -3,23 +3,28 @@
 
 #include "../model/scenario.h"
 #include "../settings/settings.h"
+#include "tile.h"
 #include <stdint.h>
+#include <stdio.h>
 
 typedef struct Board {
     UserSettings * user_settings;
+    Scenario * scenario;
 
     uint8_t width, height;
-    Piece ** tiles;
+    Tile ** tiles;
 
     Team * teams;
     uint8_t team_count;
 
-    uint8_t turn;
+    uint8_t active_turn;
 } Board;
 
 Board * createBoard(Scenario * scenario, UserSettings * settings);
 void renderBoardWithSelection(Board * board, int pos_x, int pos_y, int i, int j, int w, int h, int sel_x, int sel_y);
 void renderBoard(Board * board, int pos_x, int pos_y, int i, int j, int w, int h);
 void renderScenario(Scenario * scenario, UserSettings * settings, int pos_x, int pos_y, int i, int j, int w, int h);
+void saveBoard(Board * board, FILE * stream);
+Board * loadBoard(UserSettings * settings, FILE * stream);
 
 #endif
