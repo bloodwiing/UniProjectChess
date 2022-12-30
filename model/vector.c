32314b09a3a7f26fea967caecc1d2d0b2483afeb
BLOODWIING <bandym555@gmail.com>
2022-12-31 01:01:59 +0200

    Reduce pointers of Vectors

--- model/vector.c
+++ model/vector.c
@@ -1,23 +1,11 @@
 #include <stdlib.h>
 #include "vector.h"
 
-Vector * createVector(int16_t x, int16_t y) {
-    Vector * out = calloc(1, sizeof(Vector));
-    out->x = x;
-    out->y = y;
+Vector createVector(int16_t x, int16_t y) {
+    return (Vector){.x = x, .y = y};
 }
 
-void saveVector(Vector * vector, FILE * stream) {
-    fwrite(vector, sizeof(Vector), 1, stream);
-}
-
-Vector * loadVector(FILE * stream) {
-    Vector * out = malloc(sizeof(Vector));
-    fread(out, sizeof(Vector), 1, stream);
-    return out;
-}
-
-Vector * normaliseCoordinates(int16_t x, int16_t y, TeamDirection direction) {
+Vector normaliseCoordinates(int16_t x, int16_t y, TeamDirection direction) {
     switch (direction) {
         case TeamDirectionUp:
             return createVector(x, y);
@@ -33,6 +21,10 @@ Vector * normaliseCoordinates(int16_t x, int16_t y, TeamDirection direction) {
     }
 }
 
-Vector * normaliseVector(Vector * vector, TeamDirection direction) {
-    return normaliseCoordinates(vector->x, vector->y, direction);
+Vector normaliseVector(Vector vector, TeamDirection direction) {
+    return normaliseCoordinates(vector.x, vector.y, direction);
+}
+
+bool_t compVectors(Vector a, Vector b) {
+    return a.x == b.x && a.y == b.y;
 }
--- model/vector.h
+++ model/vector.h
@@ -3,16 +3,18 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include "defs.h"
 #include "teamdirection.h"
 
 typedef struct Vector {
     int16_t x, y;
 } Vector;
 
-Vector * createVector(int16_t x, int16_t y);
-void saveVector(Vector * vector, FILE * stream);
-Vector * loadVector(FILE * stream);
-Vector * normaliseCoordinates(int16_t x, int16_t y, TeamDirection direction);
-Vector * normaliseVector(Vector * vector, TeamDirection direction);
+Vector createVector(int16_t x, int16_t y);
+
+Vector normaliseCoordinates(int16_t x, int16_t y, TeamDirection direction);
+Vector normaliseVector(Vector vector, TeamDirection direction);
+
+bool_t compVectors(Vector a, Vector b);
 
 #endif //CHESS_VECTOR_H
